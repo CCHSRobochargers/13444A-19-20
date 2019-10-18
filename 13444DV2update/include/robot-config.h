e218ab595ca3a15e22464449f2b3e416a0acf3f4
CCHSRoboChargers <cchsrobochargers@gmail.com>
2019-10-17 21:29:14 -0400

    enum
    added squid program to spin down, up, and stop
    
    --te

--- 13444DV2update/include/robot-config.h
+++ 13444DV2update/include/robot-config.h
@@ -4,8 +4,10 @@ extern brain Brain;
 
 // VEXcode devices
 extern controller Controller1;
-extern drivetrain Drivetrain;
-extern motor Motor9;
+extern motor LeftFront;
+extern motor rightFront;
+extern motor leftBack;
+extern motor rightBack;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
